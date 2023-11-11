0e717a8030b492141f4ad618c1abb416a668e16c
AlvirElWapo <correodeandresalvir@gmail.com>
2023-11-11 09:19:22 -0600

    PROYECTO ARREGLADO

--- script_montaje.sh
+++ script_montaje.sh
@@ -1,29 +1,22 @@
 #Script para realizar desde el punto 1 hasta el 6 del archivo guía de Andrés para la creación de una llamada al sistema
 #FAVOR DE MODIFICAR EL CÓDIGO SEGÚN SEA NECESARIO!!!!
 
-
-
 # Descarga del kernel y descomprime el mismo...
-curl -o ./Kernel_De_Linux.tar.xz "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.259.tar.xz"
+curl -o ./Kernel_De_Linux.tar.xz "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.138.tar.xz"
 tar -xJf Kernel_De_Linux.tar.xz -C ./
 
 #Creación de los archivos necesarios para las syscalls...
-mkdir ./linux-5.4.259/terejaker 
-touch ./linux-5.4.259/terejaker/tere_syscall.c 
+mkdir ./linux-5.15.138/terejaker
+touch ./linux-5.15.138/terejaker/tere_syscall.c
 
 #Programa para realizar operaciones según una entrada...
 echo '
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 
-SYSCALL_DEFINE3(teresyscall, char*, operation, char*, str1, char*, str2)
+SYSCALL_DEFINE3(teresyscall, char*, operation, int, num1, int, num2)
 {
-    long num1, num2, result;
-    
-    if (kstrtol(str1, 10, &num1) || kstrtol(str2, 10, &num2)) {
-        printk("Entrada INCORRECTA: Ambas entradas deben ser de tipo INT.\n");
-        return -EINVAL;
-    }
+    long result;
 
     if (strcmp(operation, "suma") == 0) {
         result = num1 + num2;
@@ -44,12 +37,16 @@ SYSCALL_DEFINE3(teresyscall, char*, operation, char*, str1, char*, str2)
 
     printk("RESULTADO EN EL KRB: %ld\n", result);
     pr_info("RESULTADO EN CONSOLA: %ld\n", result);
-    
+
     return result;
 }
-' >> ./linux-5.4.259/terejaker/tere_syscall.c
+' >> ./linux-5.15.138/terejaker/tere_syscall.c
 
-echo 'obj-y := teresyscall.o' >> ./linux-5.4.259/terejaker/Makefile
-sed -i '1055s|$|terejaker/|' linux-5.4.259/Makefile 
-sed -i '1423i asmlinkage long sys_teresyscall(char* str);' ./linux-5.4.259/include/linux/syscalls.h
-echo '548 common teresyscall sys_teresyscall' >> ./linux-5.4.259/arch/x86/entry/syscalls/syscall_64.tbl
+echo 'obj-y := teresyscall.o' > ./linux-5.15.138/terejaker/Makefile
+echo 'teresyscall-y := tere_syscall.o' >> ./linux-5.15.138/terejaker/Makefile
+
+sed -i '1162s|$| terejaker/|' linux-5.15.138/Makefile
+
+sed -i '1384i asmlinkage long sys_teresyscall(char* operation,int num1, int num2);' ./linux-5.15.138/include/linux/syscalls.h
+
+echo '548 common teresyscall sys_teresyscall' >> ./linux-5.15.138/arch/x86/entry/syscalls/syscall_64.tbl
